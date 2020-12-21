dda1da58c3b9558ce59f2fffde16f9f5fd2a9082
Albin Sander <51294735+Albin-Sander@users.noreply.github.com>
2020-12-21 23:38:02 +0100

    Fixe some things

--- Script/script.js
+++ Script/script.js
@@ -4,7 +4,7 @@ var doughnut = 0;
      window.setInterval(
      function () {
          doughnut = doughnut + 1;
-          let updateSpin = document.getElementById("putinSpinText").innerHTML = "You have spun around Putin " + doughnut + " times!";
+          let updateSpin = document.getElementById("putinSpinText").innerHTML = "You have spun around Almis " + doughnut + " times!";
           
 
      }, 8000);
